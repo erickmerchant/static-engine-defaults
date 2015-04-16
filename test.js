cdb73d94324012e8c05666b67f2991d4ad0812da
Erick Merchant <erickmerchant@gmail.com>
2015-04-16 00:19:48 -0700

    Add test coverage

--- test.js
+++ test.js
@@ -10,10 +10,7 @@ describe('plugin', function () {
     var defaults = plugin('./test/content/defaults.json', JSON.parse)
 
     defaults([{}, {}], function (err, pages) {
-      if (err) {
-        throw err
-      }
-
+      assert.equal(null, err)
       assert.deepEqual(pages, [
         {alpha: 'a', beta: 'b'},
         {alpha: 'a', beta: 'b'}
@@ -27,10 +24,7 @@ describe('plugin', function () {
     var defaults = plugin('./test/content/defaults.json', JSON.parse)
 
     defaults([{alpha: 1}, {alpha: 2}], function (err, pages) {
-      if (err) {
-        throw err
-      }
-
+      assert.equal(null, err)
       assert.deepEqual(pages, [
         {alpha: 1, beta: 'b'},
         {alpha: 2, beta: 'b'}
@@ -39,15 +33,49 @@ describe('plugin', function () {
       done()
     })
   })
+
+  it('it should handle errors from fs.readFile', function (done) {
+    var plugin = mock({
+      './test/content/defaults.json': '{ "alpha": "a", "beta": "b" }'
+    }, {
+      fs: new Error('fs.readFile error!')
+    })
+    var defaults = plugin('./test/content/defaults.json', JSON.parse)
+
+    defaults([{}, {}], function (err) {
+      assert.equal('fs.readFile error!', err.message)
+
+      done()
+    })
+  })
+
+  it('it should handle errors from the converter', function (done) {
+    var plugin = mock({
+      './test/content/defaults.json': '{ "alpha": "a", "beta": "b" }'
+    })
+    var defaults = plugin('./test/content/defaults.json', function () {
+      throw new Error('parse error!')
+    })
+
+    defaults([{}, {}], function (err) {
+      assert.equal('parse error!', err.message)
+
+      done()
+    })
+  })
 })
 
-function mock (files) {
+function mock (files, errors) {
   var rewire = require('rewire')
   var plugin = rewire('./index.js')
 
   plugin.__set__('fs', {
     readFile: function (filename, options, callback) {
-      callback(null, files[filename])
+      if (errors && errors.fs) {
+        callback(errors.fs)
+      } else {
+        callback(null, files[filename])
+      }
     }
   })
 
